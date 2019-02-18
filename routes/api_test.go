287277712f18e0e8bf23535cec5775a5a3bd34a8
David Kröll <david.kroell@outlook.com>
2019-02-18 09:00:43 +0100

    Add API test
    Signed-off-by: David Kröll <david.kroell@outlook.com>

--- routes/api_test.go
+++ routes/api_test.go
@@ -1,6 +1,8 @@
 package routes
 
 import (
+	"encoding/json"
+	"io"
 	"net/http"
 	"net/http/httptest"
 	"testing"
@@ -31,5 +33,42 @@ func TestHealthHandler(t *testing.T) {
 }
 
 func TestNewGameHandler(t *testing.T) {
+	reader, writer := io.Pipe()
 
+	body := NewGameBody{
+		Name:  "testgame",
+		Owner: "testuser",
+	}
+
+	go func() {
+		defer writer.Close()
+
+		if err := json.NewEncoder(writer).Encode(&body); err != nil {
+			t.Errorf(err.Error())
+		}
+	}()
+
+	req, err := http.NewRequest("POST", "", reader)
+
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	recorder := httptest.NewRecorder()
+	hf := http.HandlerFunc(NewGameHandler)
+
+	hf.ServeHTTP(recorder, req)
+
+	if status := recorder.Code; status != http.StatusOK {
+		t.Errorf("handler returned wrong status code: got %v want %v",
+			status, http.StatusOK)
+	}
+
+	response := Response{}
+	if err := json.NewDecoder(recorder.Body).Decode(&response); err != nil {
+		t.Error(err)
+	}
+	if !response.Success {
+		t.Errorf("handler returned unexpected body; success not true: got %v", response)
+	}
 }
